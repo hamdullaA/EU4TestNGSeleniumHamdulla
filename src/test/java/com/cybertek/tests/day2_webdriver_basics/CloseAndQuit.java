eb8f9e347c4f2678706e3039cd4f3161c311dde3
Hamdulla.A <75882853+hamdullaA@users.noreply.github.com>
2021-01-16 13:26:59 +0100

    updated

--- src/test/java/com/cybertek/tests/day2_webdriver_basics/CloseAndQuit.java
+++ src/test/java/com/cybertek/tests/day2_webdriver_basics/CloseAndQuit.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CloseAndQuit {
-
+//
     public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
